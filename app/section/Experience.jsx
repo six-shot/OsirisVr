cfbb30f207e082b2155a9b0e65d031cab4bbad88
six-shot <okhuomonajayi54@gmail.com>
2024-12-09 19:51:10 +0100

    fix

--- app/section/Experience.jsx
+++ app/section/Experience.jsx
@@ -14,18 +14,69 @@ export default function Experience() {
             <h2 className="text-[54px] font-[family-name:var(--font-neue)]">
               VR EXPERIENCES
             </h2>
-            <div className="relative ">
-              <button className="btn text-sm uppercase h-[44px] w-[9.62rem] text-white   font-roboto_mono border-[2px] border-[#67e9cb]/20">
-                Try it now
-              </button>
-              <div className="cornerr-1" />
-              <div className="cornerr-2" />
-              <div className="cornerr-3" />
-              <div className="cornerr-4" />
-              <div className="cornerr-5" />
-              <div className="cornerr-6" />
-              <div className="cornerr-7" />
-              <div className="cornerr-8" />
+            <div className="flex items-center gap-10 pr-10">
+              <div className="flex items-center">
+                <Image
+                  src="/arrow2.svg"
+                  width={15}
+                  height={31}
+                  unoptimized
+                  alt="experience"
+                />
+                <Image
+                  src="/arrow1.svg"
+                  width={15}
+                  height={31}
+                  unoptimized
+                  alt="experience"
+                />
+                <Image
+                  src="/arrow.svg"
+                  width={15}
+                  height={31}
+                  unoptimized
+                  alt="experience"
+                />
+              </div>
+              <div className="relative ">
+                <button className="btn text-sm uppercase h-[44px] w-[9.62rem] text-white   font-roboto_mono border-[2px] border-[#67e9cb]/20">
+                  Try it now
+                </button>
+                <div className="cornerr-1" />
+                <div className="cornerr-2" />
+                <div className="cornerr-3" />
+                <div className="cornerr-4" />
+                <div className="cornerr-5" />
+                <div className="cornerr-6" />
+                <div className="cornerr-7" />
+                <div className="cornerr-8" />
+              </div>
+              <div className="flex items-center">
+                <Image
+                  src="/arrow.svg"
+                  width={15}
+                  height={31}
+                  className="rotate-[180deg]"
+                  unoptimized
+                  alt="experience"
+                />
+                <Image
+                  src="/arrow1.svg"
+                  width={15}
+                  height={31}
+                  className="rotate-[180deg]"
+                  unoptimized
+                  alt="experience"
+                />
+                <Image
+                  src="/arrow2.svg"
+                  width={15}
+                  height={31}
+                  className="rotate-[180deg]"
+                  unoptimized
+                  alt="experience"
+                />
+              </div>
             </div>
           </div>
         </Marquee>
